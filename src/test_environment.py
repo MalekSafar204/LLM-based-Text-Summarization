48a7b9976f3c7ac060645ccc588d041165e5fecb
Malek Safar <maleksafar58@gmail.com>
2026-03-13 15:04:08 +0200

    environment setup

---
+++ src/test_environment.py
@@ -0,0 +1,7 @@
+import torch
+import transformers
+import datasets
+
+print("Environment ready")
+print("Torch version:", torch.__version__)
+print("Transformers version:", transformers.__version__)
